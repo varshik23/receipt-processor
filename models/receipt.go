1efef60b6348ec2a8ca990851a1e2056f98a3049
varshik23 <sontivarshik@gmail.com>
2024-01-27 15:23:56 -0700

    Added validations for the data schemas

--- models/receipt.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type receipt struct {
-	Retailer string `json:"retailer"`
-	PurchaseDate string `json:"purchaseDate"`
-	PurchaseTime string `json:"purchaseTime"`
-	Items []item `json:"items"`
-	Total string `json:"total"`
-}
---
+++ models/receipt_models.go
@@ -0,0 +1,19 @@
+package models
+
+type Receipt struct {
+	Retailer string `json:"retailer" binding:"required"`
+	PurchaseDate string `json:"purchaseDate" binding:"required"`
+	PurchaseTime string `json:"purchaseTime" binding:"required"`
+	Items []Item `json:"items" binding:"required,dive"`
+	Total string `json:"total" binding:"required,numeric"`
+}
+
+type Item struct {
+	ShortDescription string `json:"shortDescription" binding:"required"`
+	Price string `json:"price" binding:"required,numeric"`
+}
+
+type ErrorMsg struct {
+    Field string `json:"field"`
+    Message   string `json:"message"`
+}
